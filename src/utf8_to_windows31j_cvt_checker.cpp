34e94fac5fabe1332e9ae06eec84199e9bb011f3
yumetodo <yume-wikijp@live.jp>
2021-09-24 18:16:12 +0900

    feat: support multi file

--- src/utf8_to_windows31j_cvt_checker.cpp
+++ src/utf8_to_windows31j_cvt_checker.cpp
@@ -1,30 +1,36 @@
-﻿#include <iostream>
-#include <fstream>
+﻿#include <fstream>
+#include <iostream>
 #include <utf8_to_windows31j_cvt_checker.hpp>
-void lint(std::istream& is)
-{
+bool lint(std::istream& is, const char* path = nullptr) {
     iconv_utf8_windows31j_cvt cvt;
     std::size_t line{};
     bool has_error = false;
-    for(std::string buf; std::getline(is, buf); ++line){
+    for (std::string buf; std::getline(is, buf); ++line) {
         try {
             cvt(buf);
-        }
-        catch(const std::runtime_error& er) {
+        } catch (const std::runtime_error& er) {
             if (!std::exchange(has_error, true)) {
-                std::cerr << ascii_color::bright_red << "invalid input(s) has detected. see blow:\n";
+                std::cerr << ascii_color::bright_red
+                          << "invalid input(s) has detected. see blow:\n";
+                if (path) {
+                    std::cerr << ascii_color::reset << "In file " << path << '\n';
+                }
             }
-            std::cerr << ascii_color::bright_cyan << "line " << line << ascii_color::reset << '\n' << buf << std::endl;
+            std::cerr << ascii_color::bright_cyan << "line " << line << ascii_color::reset << '\n'
+                      << buf << std::endl;
         }
     }
+    return true;
 }
-int main(int argc, char** argv)
-{
-    if (argc == 2) {
-        std::ifstream ifs(argv[1]);
-        lint(ifs);
+int main(int argc, char** argv) {
+    if (argc == 1) {
+        return lint(std::cin) ? 0 : 1;
     }
-    else {
-        lint(std::cin);
+
+    int ret = 0;
+    for (int i = 1; i < argc; i++) {
+        std::ifstream ifs(argv[i]);
+        if (lint(ifs, argv[i])) ret = 1;
     }
+    return ret;
 }
