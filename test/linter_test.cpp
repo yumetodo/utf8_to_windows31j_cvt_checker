eda5c9f517c86e49013ee00ef8ef166baf743707
yumetodo <yume-wikijp@live.jp>
2021-10-08 01:23:37 +0900

    feat(test): add line number check

--- test/linter_test.cpp
+++ test/linter_test.cpp
@@ -18,3 +18,26 @@ IUTEST(Linter, fail) {
     std::ostringstream oss;
     IUTEST_ASSERT_EQ(false, lint(iss, oss));
 }
+IUTEST(Linter, OneBasedIndex) {
+    constexpr auto input = u8R"(1人,👨
+2人,👨‍👩‍
+3人,👨‍👩‍👦
+4人,👨‍👩‍👧‍👦)";
+    std::stringstream iss;
+    iss << reinterpret_cast<const char*>(input);
+    std::stringstream oss;
+    lint(iss, oss);
+    std::string buf;
+    IUTEST_ASSERT(std::getline(oss, buf)); // skip
+    IUTEST_ASSERT(std::getline(oss, buf));
+    IUTEST_ASSERT(std::string::npos != buf.find("line 1"));
+    IUTEST_ASSERT(std::getline(oss, buf)); // skip
+    IUTEST_ASSERT(std::getline(oss, buf));
+    IUTEST_ASSERT(std::string::npos != buf.find("line 2"));
+    IUTEST_ASSERT(std::getline(oss, buf)); // skip
+    IUTEST_ASSERT(std::getline(oss, buf));
+    IUTEST_ASSERT(std::string::npos != buf.find("line 3"));
+    IUTEST_ASSERT(std::getline(oss, buf)); // skip
+    IUTEST_ASSERT(std::getline(oss, buf));
+    IUTEST_ASSERT(std::string::npos != buf.find("line 4"));
+}
