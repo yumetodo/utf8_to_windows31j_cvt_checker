b5a2c16900e9c3ea43067f8aafb752e267008da8
yumetodo <yume-wikijp@live.jp>
2021-10-11 01:10:30 +0900

    feat: support -h, --help

--- src/utf8_to_windows31j_cvt_checker.cpp
+++ src/utf8_to_windows31j_cvt_checker.cpp
@@ -1,4 +1,4 @@
-/*=============================================================================
+﻿/*=============================================================================
   Copyright (C) 2021 yumetodo <yume-wikijp@live.jp>
   Distributed under the Boost Software License, Version 1.0.
   (See https://www.boost.org/LICENSE_1_0.txt)
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <iostream>
 #include <linter.hpp>
+#include <string_view>
 #include <utf8_to_windows31j_cvt_checker.hpp>
 
 constexpr auto help_text = R"(utf8_to_windows31j_cvt_checker
@@ -16,8 +17,19 @@ Usage:
 )";
 
 int main(int argc, char** argv) {
+    using namespace std::string_view_literals;
+    if (argc < 1) {
+        return 2;
+    }
     if (argc == 1) {
         return lint(std::cin) ? 0 : 1;
+    }
+    if (argc == 2) {
+        const std::string_view maybe_help_request = argv[1];
+        if (maybe_help_request == "--help"sv || maybe_help_request == "-h"sv) {
+            std::cerr << help_text << std::flush;
+            return 0;
+        }
     }
 
     int ret = 0;
